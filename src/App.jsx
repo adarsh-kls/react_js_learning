bd2a4e2eceb5ff74d106dfadb625b1e6891ef6b0
adarsh-cyber08 <adarsh@atvisai.in>
2026-05-19 19:28:05 +0530

    this is how the hook in react are used

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React,{useState} from 'react'
+
+const App = () => {
+  const [num, setnum] = useState(0)
+
+  function increaseNum() {
+    setnum(num+1)
+  }
+  function decreaseNum() {
+    setnum(num-1)
+  }
+  function jump5Num() {
+    setnum(num+5)
+  }
+
+  return(
+    <div>
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>increase</button>
+      <button onClick={decreaseNum}>decrease</button>
+      <button onClick={jump5Num}>increase by 5</button>
+    </div>
+  )
+}
+
+export default App
